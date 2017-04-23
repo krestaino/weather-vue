96a0e5fe8091770600612e8dd277e9c46002dd76
Kevin Restaino <kevinrestaino@gmail.com>
2017-04-23 01:23:53 -0500

    Fetch weather

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,279 @@
 <template>
-  <div id="app">
-    <hello></hello>
+  <div id="current">
+    <div>{{ darkskyResponse.currently.temperature }}</div>
+    <button v-on:click="fetchWeather">Fetch Weather</button>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
-
 export default {
-  name: 'app',
-  components: {
-    Hello
+  name: 'current',
+  methods: {
+    fetchWeather: function() {
+      fetch(this.darkskyEndpoint)
+        .then(  
+          function(response) {
+            if (response.status !== 200) {  
+              console.log('Looks like there was a problem. Status Code: ' +
+                response.status);
+              return;
+            }
+
+            response.json().then(function(data) {
+              this.darkskyResponse = data;
+            }.bind(this));
+          }.bind(this)
+        )  
+        .catch(function(err) {
+          console.log('Fetch Error :-S', err);
+        });
+    }
+  },
+  data () {
+    return {
+      darkskyEndpoint: 'http://localhost:420/weather/v1/42.0457/-87.8501',
+      darkskyResponse: {
+        currently: {
+          time: '',
+          summary: '',
+          icon: '',
+          nearestStormDistance: '',
+          nearestStormBearing: '',
+          precipIntensity: '',
+          precipProbability: '',
+          temperature: '',
+          apparentTemperature: '',
+          dewPoint: '',
+          humidity: '',
+          windSpeed: '',
+          windBearing: '',
+          visibility: '',
+          cloudCover: '',
+          pressure: '',
+          ozone: ''
+        },
+        daily: {
+          data: [
+            {
+              time: '',
+              summary: '',
+              icon: '',
+              sunriseTime: '',
+              sunsetTime: '',
+              moonPhase: '',
+              precipIntensity: '',
+              precipIntensityMax: '',
+              precipProbability: '',
+              temperatureMin: '',
+              temperatureMinTime: '',
+              temperatureMax: '',
+              temperatureMaxTime: '',
+              apparentTemperatureMin: '',
+              apparentTemperatureMinTime: '',
+              apparentTemperatureMax: '',
+              apparentTemperatureMaxTime: '',
+              dewPoint: '',
+              humidity: '',
+              windSpeed: '',
+              windBearing: '',
+              visibility: '',
+              cloudCover: '',
+              pressure: '',
+              ozone: ''
+            },
+            {
+              time: '',
+              summary: '',
+              icon: '',
+              sunriseTime: '',
+              sunsetTime: '',
+              moonPhase: '',
+              precipIntensity: '',
+              precipIntensityMax: '',
+              precipProbability: '',
+              temperatureMin: '',
+              temperatureMinTime: '',
+              temperatureMax: '',
+              temperatureMaxTime: '',
+              apparentTemperatureMin: '',
+              apparentTemperatureMinTime: '',
+              apparentTemperatureMax: '',
+              apparentTemperatureMaxTime: '',
+              dewPoint: '',
+              humidity: '',
+              windSpeed: '',
+              windBearing: '',
+              visibility: '',
+              cloudCover: '',
+              pressure: '',
+              ozone: ''
+            },
+            {
+              time: '',
+              summary: '',
+              icon: '',
+              sunriseTime: '',
+              sunsetTime: '',
+              moonPhase: '',
+              precipIntensity: '',
+              precipIntensityMax: '',
+              precipProbability: '',
+              temperatureMin: '',
+              temperatureMinTime: '',
+              temperatureMax: '',
+              temperatureMaxTime: '',
+              apparentTemperatureMin: '',
+              apparentTemperatureMinTime: '',
+              apparentTemperatureMax: '',
+              apparentTemperatureMaxTime: '',
+              dewPoint: '',
+              humidity: '',
+              windSpeed: '',
+              windBearing: '',
+              visibility: '',
+              cloudCover: '',
+              pressure: '',
+              ozone: ''
+            },
+            {
+              time: '',
+              summary: '',
+              icon: '',
+              sunriseTime: '',
+              sunsetTime: '',
+              moonPhase: '',
+              precipIntensity: '',
+              precipIntensityMax: '',
+              precipProbability: '',
+              temperatureMin: '',
+              temperatureMinTime: '',
+              temperatureMax: '',
+              temperatureMaxTime: '',
+              apparentTemperatureMin: '',
+              apparentTemperatureMinTime: '',
+              apparentTemperatureMax: '',
+              apparentTemperatureMaxTime: '',
+              dewPoint: '',
+              humidity: '',
+              windSpeed: '',
+              windBearing: '',
+              visibility: '',
+              cloudCover: '',
+              pressure: '',
+              ozone: ''
+            },
+            {
+              time: '',
+              summary: '',
+              icon: '',
+              sunriseTime: '',
+              sunsetTime: '',
+              moonPhase: '',
+              precipIntensity: '',
+              precipIntensityMax: '',
+              precipProbability: '',
+              temperatureMin: '',
+              temperatureMinTime: '',
+              temperatureMax: '',
+              temperatureMaxTime: '',
+              apparentTemperatureMin: '',
+              apparentTemperatureMinTime: '',
+              apparentTemperatureMax: '',
+              apparentTemperatureMaxTime: '',
+              dewPoint: '',
+              humidity: '',
+              windSpeed: '',
+              windBearing: '',
+              visibility: '',
+              cloudCover: '',
+              pressure: '',
+              ozone: ''
+            },
+            {
+              time: '',
+              summary: '',
+              icon: '',
+              sunriseTime: '',
+              sunsetTime: '',
+              moonPhase: '',
+              precipIntensity: '',
+              precipIntensityMax: '',
+              precipProbability: '',
+              temperatureMin: '',
+              temperatureMinTime: '',
+              temperatureMax: '',
+              temperatureMaxTime: '',
+              apparentTemperatureMin: '',
+              apparentTemperatureMinTime: '',
+              apparentTemperatureMax: '',
+              apparentTemperatureMaxTime: '',
+              dewPoint: '',
+              humidity: '',
+              windSpeed: '',
+              windBearing: '',
+              visibility: '',
+              cloudCover: '',
+              pressure: '',
+              ozone: ''
+            },
+            {
+              time: '',
+              summary: '',
+              icon: '',
+              sunriseTime: '',
+              sunsetTime: '',
+              moonPhase: '',
+              precipIntensity: '',
+              precipIntensityMax: '',
+              precipProbability: '',
+              temperatureMin: '',
+              temperatureMinTime: '',
+              temperatureMax: '',
+              temperatureMaxTime: '',
+              apparentTemperatureMin: '',
+              apparentTemperatureMinTime: '',
+              apparentTemperatureMax: '',
+              apparentTemperatureMaxTime: '',
+              dewPoint: '',
+              humidity: '',
+              windSpeed: '',
+              windBearing: '',
+              visibility: '',
+              cloudCover: '',
+              pressure: '',
+              ozone: ''
+            },
+            {
+              time: '',
+              summary: '',
+              icon: '',
+              sunriseTime: '',
+              sunsetTime: '',
+              moonPhase: '',
+              precipIntensity: '',
+              precipIntensityMax: '',
+              precipProbability: '',
+              temperatureMin: '',
+              temperatureMinTime: '',
+              temperatureMax: '',
+              temperatureMaxTime: '',
+              apparentTemperatureMin: '',
+              apparentTemperatureMinTime: '',
+              apparentTemperatureMax: '',
+              apparentTemperatureMaxTime: '',
+              dewPoint: '',
+              humidity: '',
+              windSpeed: '',
+              windBearing: '',
+              visibility: '',
+              cloudCover: '',
+              pressure: '',
+              ozone: ''
+            }
+          ]
+        }
+      }
+    }
   }
 }
 </script>
