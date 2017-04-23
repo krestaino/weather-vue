05d9075a2787857a89a1119ed625c8a7eae6b24c
Kevin Restaino <kevinrestaino@gmail.com>
2017-04-23 00:59:08 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// The following line loads the standalone build of Vue instead of the runtime-only build,
+// so you don't have to do: import Vue from 'vue/dist/vue'
+// This is done with the browser options. For the config, see package.json
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({ // eslint-disable-line no-new
+  el: '#app',
+  render: (h) => h(App)
+})
