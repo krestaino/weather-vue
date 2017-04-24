c6c17ec7660a4ca2b640fb99a6d7550eb50f3360
Kevin Restaino <kevinrestaino@gmail.com>
2017-04-24 03:22:11 -0500

    Styling

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,53 @@
 <template>
   <div id="weather">
-    <div class="inner">
-      <div class="controls">
+    <div class="inner fadeIn">
+      <div class="search">
         <input autofocus type="text" v-model="inputQuery" v-on:keyup.enter="fetchGeolocation">
+        <button v-on:click="fetchGeolocation">Search</button>
         <button v-on:click="browerGeolocation">Find My Location</button>
-        <button v-on:click="fetchWeather">Refresh</button>
-        <div v-if="darkskyResponse.currently">Last updated: {{ darkskyResponse.currently.time * 1000 | moment("h:mm:ss A") }}</div>
       </div>
 
-      <div v-if="darkskyResponse.currently">
-        <div class="current">
-          <div class="row">
-            <div class="col">
-              <div v-bind:data-icon="darkskyResponse.currently.icon" class="icon"></div>
-              <div v-if="geocodingResponse[0]">{{ geocodingResponse[0].city }}, {{ geocodingResponse[0].administrativeLevels.level1short }}</div>
-              <div>{{ Math.round(darkskyResponse.currently.temperature) }} °F</div>
-              <div>{{ darkskyResponse.currently.time * 1000 | moment("dddd, MMMM Do") }}</div>
-              <div>{{ darkskyResponse.currently.summary }}</div>
-            </div>
+      <div class="weather">
+        <div class="weather-inner fadeIn" v-if="darkskyResponse.currently">
+          <div class="current">
+            <div class="row">
+              <div class="col">
+                <div class="location" v-if="geocodingResponse[0]">{{ geocodingResponse[0].city }}, {{ geocodingResponse[0].administrativeLevels.level1short }} <span>{{ geocodingResponse[0].zipcode }}</span></div>
+                <div>{{ darkskyResponse.currently.time * 1000 | moment("dddd, MMMM Do") }}</div>
+                <div>{{ darkskyResponse.currently.summary }}</div>
+                <div v-bind:data-icon="darkskyResponse.currently.icon" class="icon"></div>
+                <div>{{ Math.round(darkskyResponse.currently.temperature) }}°F</div>
+              </div>
 
-            <div class="col details">
-              <div>Precipitation: {{ darkskyResponse.currently.precipProbability }}%</div>
-              <div>Cloud Coverage: {{ darkskyResponse.currently.cloudCover }}%</div>
-              <div>Humidity: {{ darkskyResponse.currently.humidity }}%</div>
-              <div>Dew Point: {{ Math.round(darkskyResponse.currently.dewPoint) }} °F</div>
-              <div>Wind: {{ darkskyResponse.currently.windSpeed }} mph</div>
-              <div>Visibility: {{ darkskyResponse.currently.visibility }} miles</div>
-              <div>Sunrise: {{ darkskyResponse.daily.data[0].sunriseTime * 1000 | moment("h:mm A") }}</div>
-              <div>Sunset: {{ darkskyResponse.daily.data[0].sunsetTime * 1000 | moment("h:mm A") }}</div>
+              <div class="col details">
+                <div>Precipitation: {{ darkskyResponse.currently.precipProbability }}%</div>
+                <div>Cloud Coverage: {{ darkskyResponse.currently.cloudCover }}%</div>
+                <div>Humidity: {{ darkskyResponse.currently.humidity }}%</div>
+                <div>Dew Point: {{ Math.round(darkskyResponse.currently.dewPoint) }} °F</div>
+                <div>Wind: {{ darkskyResponse.currently.windSpeed }} mph</div>
+                <div>Visibility: {{ darkskyResponse.currently.visibility }} miles</div>
+                <div>Sunrise: {{ darkskyResponse.daily.data[0].sunriseTime * 1000 | moment("h:mm A") }}</div>
+                <div>Sunset: {{ darkskyResponse.daily.data[0].sunsetTime * 1000 | moment("h:mm A") }}</div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div class="forecast">
-          <ul class="days">
-            <li class="day" v-for="day in darkskyResponse.daily.data">
-              <div>{{ day.time * 1000 | moment("ddd") }}</div>
-              <div v-bind:data-icon="day.icon" class="icon"></div>
-              <div>{{ Math.round(day.temperatureMax) }} °F</div>
-              <div>{{ Math.round(day.temperatureMin) }} °F</div>
-            </li>
-          </ul>
+          <div class="forecast">
+            <ul class="days">
+              <li class="day" v-for="day in darkskyResponse.daily.data">
+                <div>{{ day.time * 1000 | moment("ddd") }}</div>
+                <div v-bind:data-icon="day.icon" class="icon"></div>
+                <div><strong>{{ Math.round(day.temperatureMax) }}°</strong></div>
+                <div>{{ Math.round(day.temperatureMin) }}°</div>
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
+
+      <div class="refresh">
+        <button v-on:click="fetchWeather">Refresh</button>
+        <div class="last fadeIn" v-if="darkskyResponse.currently">Last updated: {{ darkskyResponse.currently.time * 1000 | moment("h:mm:ss A") }}</div>
       </div>
     </div>
 
@@ -180,7 +186,17 @@ export default {
 
 <style lang="scss">
 html, body {
+  font-family: 'Roboto', sans-serif;
+  font-size: 20px;
+  font-weight: 300;
   height: 100%;
+  line-height: 1.4;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+::selection {
+  background-color: #ddd;
 }
 
 #weather {
@@ -189,40 +205,124 @@ html, body {
   flex-direction: column;
   height: 100%;
   justify-content: center;
+
+  strong {
+    font-weight: 500;
+  }
+
+  button {
+    background-color: #ddd;
+    border: 0;
+    color: #999;
+    cursor: pointer;
+    font-family: 'Roboto', sans-serif;
+    font-size: 15px;
+    font-weight: 500;
+    padding: 15px;
+    transition: 0.3s;
+
+    &:hover {
+      background-color: #ccc;
+      color: #888;
+    }
+  }
+
+  input {
+    border: 1px solid #bbb;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+
+    &:focus,
+    &:hover {
+      border: 1px solid #000;
+      outline: 0;
+    }
+  }
   
   .inner {
     background-color: #fbfbfb;
+    box-shadow: 0px 0px 150px 0px rgba(0,0,0,0.5);
+    color: #777;
     display: flex;
     flex-direction: column;
+    height: 440px;
     max-width: 800px;
-    padding: 15px;
+    padding: 30px;
     position: relative;
     width: 100%;
     z-index: 1;
   }
 
   #map {
+    filter: grayscale(100);
     height: 100%;
+    opacity: 0.5;
     position: fixed !important;
     width: 100%;
   }
 
-  button {
-    border: 1px solid red;
-  }
-
-  div {
-    min-height: 20px;
-  }
-
+  .search,
+  .weather,
+  .weather-inner,
   .current,
   .forecast,
-  .controls {
+  .refresh {
     display: flex;
     flex-direction: column;
-    
-    + div {
-      margin-top: 30px;
+  }
+
+  .search {
+    flex-direction: row;
+
+    input {
+      flex: 1;
+      font-size: 20px;
+      padding: 5px;
+    }
+
+    button {
+      margin-left: 15px;
+    }
+  }
+
+  .weather {
+    flex: 1;
+    margin-top: 30px;
+
+    .weather-inner {
+      flex: 1;
+    }
+
+    .current,
+    .forecast {
+      flex: 1;
+    }
+
+    .details {
+      font-size: 18px;
+      line-height: 1;
+      padding-top: 7px;
+    }
+
+    .forecast {
+      justify-content: flex-end;
+      margin-bottom: 30px;
+
+      .day {
+        color: #aaa;
+        font-size: 16px;
+        line-height: 1.6;
+        text-align: center;
+      }
+    }
+
+    .location {
+      font-size: 32px;
+
+      span {
+        color: #999;
+        font-size: 26px;
+      }
     }
   }
 
@@ -251,6 +351,38 @@ html, body {
 
       li {
         flex: 1;
+      }
+    }
+  }
+
+  .refresh {
+    position: relative;
+
+    button {
+      margin-bottom: 29px;
+    }
+
+    .last {
+      bottom: 0;
+      color: #999;
+      font-size: 14px;
+      margin-top: 10px;
+      position: absolute;
+    }
+  }
+
+  .fadeIn {
+    animation-duration: 0.3s;
+    animation-fill-mode: forwards;
+    animation-iteration-count: 1;
+    animation-name: fadeIn;
+
+    @keyframes fadeIn {
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
       }
     }
   }
