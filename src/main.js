247fdf8d2df9cc2a525505c9d85cd41096ff86e5
Kevin Restaino <kevinrestaino@gmail.com>
2017-04-24 10:52:34 -0500

    Resolves #12, added Google Analytics

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'reset-css'
 import Vue from 'vue'
+import VueMoment from 'vue-moment'
 import VeeValidate from 'vee-validate'
+import Promise from 'promise-polyfill'
+import 'whatwg-fetch'
 import App from './App'
 
 Vue.config.productionTip = false
-Vue.use(require('vue-moment'))
+Vue.use(VueMoment)
 Vue.use(VeeValidate)
+
+if (!window.Promise) {
+  window.Promise = Promise
+}
 
 /* eslint-disable no-new */
 new Vue({
