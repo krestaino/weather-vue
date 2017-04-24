e1ba2d82884575e24bed8e7935338f5ea10477b9
Kevin Restaino <kevinrestaino@gmail.com>
2017-04-23 23:35:22 -0500

    Google Maps Geocoding

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,47 @@
 <template>
-  <div id="weather" v-if="darkskyResponse.currently">
+  <div id="weather">
     <div class="inner">
-      <div class="current">
-        <div class="row">
-          <div class="col">
-            <div v-bind:data-icon="darkskyResponse.currently.icon" class="icon"></div>
-            <div>{{ Math.round(darkskyResponse.currently.temperature) }} °F</div>
-            <div>{{ darkskyResponse.currently.time * 1000 | moment("dddd, MMMM Do") }}</div>
-            <div>{{ darkskyResponse.currently.summary }}</div>
-            <div>{{ latitude }}</div>
-            <div>{{ longitude }}</div>
-          </div>
+      <div class="controls">
+        <input type="text" v-model="searchQuery" v-on:keyup.enter="fetchGeolocation">
+        <button v-on:click="browerGeolocation">Find My Location</button>
+        <button v-on:click="fetchWeather">Refresh</button>
+        <div v-if="darkskyResponse.currently">Last updated: {{ darkskyResponse.currently.time * 1000 | moment("h:mm:ss A") }}</div>
+      </div>
+      
+      <div v-if="darkskyResponse.currently">
+        <div class="current">
+          <div class="row">
+            <div class="col">
+              <div v-bind:data-icon="darkskyResponse.currently.icon" class="icon"></div>
+              <div v-if="geocodingResponse[0]">{{ geocodingResponse[0].city }}, {{ geocodingResponse[0].administrativeLevels.level1short }}</div>
+              <div>{{ Math.round(darkskyResponse.currently.temperature) }} °F</div>
+              <div>{{ darkskyResponse.currently.time * 1000 | moment("dddd, MMMM Do") }}</div>
+              <div>{{ darkskyResponse.currently.summary }}</div>
+            </div>
 
-          <div class="col details">
-            <div>Precipitation: {{ darkskyResponse.currently.precipProbability }}%</div>
-            <div>Cloud Coverage: {{ darkskyResponse.currently.cloudCover }}%</div>
-            <div>Humidity: {{ darkskyResponse.currently.humidity }}%</div>
-            <div>Dew Point: {{ Math.round(darkskyResponse.currently.dewPoint) }} °F</div>
-            <div>Wind: {{ darkskyResponse.currently.windSpeed }} mph</div>
-            <div>Visibility: {{ darkskyResponse.currently.visibility }} miles</div>
-            <div>Sunrise: {{ darkskyResponse.daily.data[0].sunriseTime * 1000 | moment("h:mm A") }}</div>
-            <div>Sunset: {{ darkskyResponse.daily.data[0].sunsetTime * 1000 | moment("h:mm A") }}</div>
+            <div class="col details">
+              <div>Precipitation: {{ darkskyResponse.currently.precipProbability }}%</div>
+              <div>Cloud Coverage: {{ darkskyResponse.currently.cloudCover }}%</div>
+              <div>Humidity: {{ darkskyResponse.currently.humidity }}%</div>
+              <div>Dew Point: {{ Math.round(darkskyResponse.currently.dewPoint) }} °F</div>
+              <div>Wind: {{ darkskyResponse.currently.windSpeed }} mph</div>
+              <div>Visibility: {{ darkskyResponse.currently.visibility }} miles</div>
+              <div>Sunrise: {{ darkskyResponse.daily.data[0].sunriseTime * 1000 | moment("h:mm A") }}</div>
+              <div>Sunset: {{ darkskyResponse.daily.data[0].sunsetTime * 1000 | moment("h:mm A") }}</div>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="forecast">
-        <ul class="days">
-          <li class="day" v-for="day in darkskyResponse.daily.data">
-            <div>{{ day.time * 1000 | moment("ddd") }}</div>
-            <div v-bind:data-icon="day.icon" class="icon"></div>
-            <div>{{ Math.round(day.temperatureMax) }} °F</div>
-            <div>{{ Math.round(day.temperatureMin) }} °F</div>
-          </li>
-        </ul>
-      </div>
-
-      <div class="controls">
-        <button v-on:click="browerGeolocation">Find my Location</button>
-        <button v-on:click="fetchWeather">Refresh</button>
-        <div>Last updated: {{ darkskyResponse.currently.time * 1000 | moment("h:mm:ss A") }}</div>
+        <div class="forecast">
+          <ul class="days">
+            <li class="day" v-for="day in darkskyResponse.daily.data">
+              <div>{{ day.time * 1000 | moment("ddd") }}</div>
+              <div v-bind:data-icon="day.icon" class="icon"></div>
+              <div>{{ Math.round(day.temperatureMax) }} °F</div>
+              <div>{{ Math.round(day.temperatureMin) }} °F</div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -49,6 +51,50 @@
 export default {
   name: 'current',
   methods: {
+    fetchGeolocation: function () {
+      fetch(this.geocodingEndpoint + this.searchQuery)
+        .then(
+          function (response) {
+            if (response.status !== 200) {
+              console.log('Looks like there was a problem. Status Code: ' +
+                response.status)
+              return
+            }
+
+            response.json().then(function (data) {
+              this.latitude = data[0].latitude
+              this.longitude = data[0].longitude
+              this.geocodingResponse = data
+              this.fetchWeather()
+            }.bind(this))
+          }.bind(this)
+        )
+        .catch(function (err) {
+          console.log('Fetch Error :-S', err)
+        })
+    },
+    fetchReverseGeolocation: function () {
+      fetch(this.reverseGeocodingEndpoint + this.latitude + '/' + this.longitude)
+        .then(
+          function (response) {
+            if (response.status !== 200) {
+              console.log('Looks like there was a problem. Status Code: ' +
+                response.status)
+              return
+            }
+
+            response.json().then(function (data) {
+              this.latitude = data[0].latitude
+              this.longitude = data[0].longitude
+              this.geocodingResponse = data
+              this.fetchWeather()
+            }.bind(this))
+          }.bind(this)
+        )
+        .catch(function (err) {
+          console.log('Fetch Error :-S', err)
+        })
+    },
     fetchWeather: function () {
       fetch(this.darkskyEndpoint + this.latitude + '/' + this.longitude)
         .then(
@@ -77,14 +123,15 @@ export default {
       function success (position) {
         this.latitude = position.coords.latitude
         this.longitude = position.coords.longitude
-        this.fetchWeather()
+        this.searchQuery = this.latitude + ' ' + this.longitude
+        this.fetchReverseGeolocation()
       }
 
       function error () {
         console.log('Unable to retrieve your location.')
       }
 
-      navigator.geolocation.getCurrentPosition(success.bind(this), error)
+      navigator.geolocation.getCurrentPosition(success.bind(this), error.bind(this))
     }
   },
   mounted: function () {
@@ -94,8 +141,12 @@ export default {
     return {
       darkskyEndpoint: 'https://api.kmr.io/weather/v1/',
       darkskyResponse: {},
+      geocodingEndpoint: 'https://api.kmr.io/geocoding/v1/geocode/',
+      geocodingResponse: {},
+      reverseGeocodingEndpoint: 'https://api.kmr.io/geocoding/v1/reverse/',
       latitude: '',
-      longitude: ''
+      longitude: '',
+      searchQuery: ''
     }
   }
 }
