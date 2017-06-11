493cee1040aae930665a7845661ae8473cf65500
Ru Chern Chong <me@ruchern.com>
2017-06-11 11:09:08 +0800

    Cleanup
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/App.vue
+++ src/App.vue
@@ -1,237 +1,236 @@
 <template>
-  <div id="app">
-    <div class="weather-card">
-      <Search ref="search"/>
+    <div id="app">
+        <div class="weather-card">
+            <Search ref="search"/>
 
-      <div class="weather" v-if="store.appStatus.state === 'loaded'"> 
-        <Current class="fadeIn"/>
-        <Forecast class="fadeIn"/>
-      </div>
+            <div class="weather" v-if="store.appStatus.state === 'loaded'">
+                <Current class="fadeIn"/>
+                <Forecast class="fadeIn"/>
+            </div>
 
-      <Credits class="credits" v-else-if="store.appStatus.state === 'credits'"/> 
+            <Credits class="credits" v-else-if="store.appStatus.state === 'credits'"/>
 
-      <div :class="store.appStatus.state" class="loading-or-error" v-else>
-        <span>{{ store.appStatus.message }}</span>
-        <div class="spinner" v-if="store.appStatus.state === 'loading'"></div>
-      </div>
+            <div :class="store.appStatus.state" class="loading-or-error" v-else>
+                <span>{{ store.appStatus.message }}</span>
+                <div class="spinner" v-if="store.appStatus.state === 'loading'"></div>
+            </div>
 
-      <div class="footer">
-        <button class="refresh" title="Refresh" @click="fetchWeather()">
-          <div v-if="store.weather.currently">
-            <IconRefresh/>
-            <span>Last updated: {{ timestamp(store.weather.currently.time * 1000, store.weather.timezone) }}</span>
-          </div>
-        </button>
+            <div class="footer">
+                <button class="refresh" title="Refresh" @click="fetchWeather()">
+                    <div v-if="store.weather.currently">
+                        <IconRefresh/>
+                        <span>Last updated: {{ timestamp(store.weather.currently.time * 1000, store.weather.timezone)
+                            }}</span>
+                    </div>
+                </button>
 
-        <button class="credits" title="Credits" @click="credits()">
-          <IconHelp/>
-        </button> 
-      </div>
+                <button class="credits" title="Credits" @click="credits()">
+                    <IconHelp/>
+                </button>
+            </div>
+        </div>
+        <Background/>
     </div>
-    <Background/>
-  </div>
 </template>
 
 <script>
-import Background from './components/Background'
-import Search from './components/Search'
-import Current from './components/Current'
-import Forecast from './components/Forecast'
-import Credits from './components/Credits'
-import IconRefresh from './assets/icons/ui/refresh.svg'
-import IconHelp from './assets/icons/ui/help.svg'
-import moment from 'moment'
-import 'moment-timezone'
+  import Background from './components/Background'
+  import Search from './components/Search'
+  import Current from './components/Current'
+  import Forecast from './components/Forecast'
+  import Credits from './components/Credits'
+  import IconRefresh from './assets/icons/ui/refresh.svg'
+  import IconHelp from './assets/icons/ui/help.svg'
+  import moment from 'moment'
+  import 'moment-timezone'
 
-export default {
-  name: 'app',
+  export default {
+    name: 'app',
 
-  components: {
-    Background,
-    Search,
-    Current,
-    Forecast,
-    Credits,
-    IconRefresh,
-    IconHelp
-  },
-
-  computed: {
-    store () {
-      return this.$store.state
-    }
-  },
-
-  methods: {
-    credits () {
-      (this.store.appStatus.state === 'credits')
-        ? this.$store.dispatch('appStatus', { state: 'loaded' })
-        : this.$store.dispatch('appStatus', { state: 'credits' })
+    components: {
+      Background,
+      Search,
+      Current,
+      Forecast,
+      Credits,
+      IconRefresh,
+      IconHelp
     },
 
-    fetchWeather () {
-      this.$store.dispatch('appStatus', { state: 'loading' })
-      this.$store.dispatch('weather').then(() => {
-        this.$store.dispatch('appStatus', { state: 'loaded' })
-      })
+    computed: {
+      store () {
+        return this.$store.state
+      }
     },
 
-    timestamp (time, zone) {
-      return moment(time).tz(zone).format('h:mm A')
-    }
-  },
+    methods: {
+      credits () {
+        (this.store.appStatus.state === 'credits')
+          ? this.$store.dispatch('appStatus', {state: 'loaded'})
+          : this.$store.dispatch('appStatus', {state: 'credits'})
+      },
 
-  mounted () {
-    (localStorage.units)
-      ? this.$store.dispatch('units', localStorage.getItem('units'))
-      : null
+      fetchWeather () {
+        this.$store.dispatch('appStatus', {state: 'loading'})
+        this.$store.dispatch('weather').then(() => {
+          this.$store.dispatch('appStatus', {state: 'loaded'})
+        })
+      },
+
+      timestamp (time, zone) {
+        return moment(time).tz(zone).format('h:mm A')
+      }
+    },
+
+    mounted () {
+      (localStorage.units) ? this.$store.dispatch('units', localStorage.getItem('units')) : null
+    }
   }
-}
 </script>
 
 <style lang="scss">
-@import 'scss/_base.scss';
-@import 'scss/partials/_spinner.scss';
+    @import 'scss/_base.scss';
+    @import 'scss/partials/_spinner.scss';
 
-#app {
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  justify-content: center;
+    #app {
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        justify-content: center;
 
-  @media(max-width: 850px) {
-    height: initial;
-  }
-}
-
-.weather-card {
-  background-color: #fbfbfb;
-  border-radius: 3px;
-  box-shadow: 0px 0px 150px 0px rgba(0,0,0,0.5);
-  color: #383842;
-  display: flex;
-  flex-direction: column;
-  margin-top: 10vh;
-  max-width: 800px;
-  min-height: 510px;
-  padding: 32px;
-  position: relative;
-  width: 100%;
-  z-index: 1;
-
-  > * {
-    display: flex;
-    flex-direction: column;
-  }
-
-  @media(max-width: 850px) {
-    border-radius: 0;
-    margin: 32px;
-    max-width: 500px;
-    padding: 16px;
-  }
-
-  @media(max-width: 550px) {
-    box-shadow: none;
-    margin: 0;
-    max-width: 100%;
-    min-height: 100vh;
-  }
-}
-
-.weather {
-  flex: 1;
-  margin-top: 16px;
-  padding-bottom: 16px;
-  position: relative;
-
-  @media(max-width: 850px) {
-    padding-bottom: 48px;
-  }
-
-  .current,
-  .forecast {
-    flex: 1;
-
-    @keyframes fadeIn {
-      0% {
-        opacity: 0;
-      }
-      100% {
-        opacity: 1;
-      }
+        @media(max-width: 850px) {
+            height: initial;
+        }
     }
 
-    svg {
-      height: auto;
-      width: 100%;
-    }
-  }
-}
+    .weather-card {
+        background-color: #fbfbfb;
+        border-radius: 3px;
+        box-shadow: 0px 0px 150px 0px rgba(0, 0, 0, 0.5);
+        color: #383842;
+        display: flex;
+        flex-direction: column;
+        margin-top: 10vh;
+        max-width: 800px;
+        min-height: 510px;
+        padding: 32px;
+        position: relative;
+        width: 100%;
+        z-index: 1;
 
-.loading-or-error {
-  align-items: center;
-  flex: 1;
-  justify-content: center;
-  position: relative;
+        > * {
+            display: flex;
+            flex-direction: column;
+        }
 
-  .spinner {
-    margin: -40px auto 0;
-  }
+        @media(max-width: 850px) {
+            border-radius: 0;
+            margin: 32px;
+            max-width: 500px;
+            padding: 16px;
+        }
 
-  span {
-    font-size: 16px;
-    height: 16px;
-    margin-top: -26px;
-    position: absolute;
-    top: 50%;
-  }
-
-  &.loading span {
-    margin-top: 14px;
-  }
-}
-
-.footer {
-  align-items: center;
-  bottom: 0;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  left: 0;
-  margin: 0;
-  opacity: .75;
-  padding: 10px;
-  position: absolute;
-  width: 100%;
-
-  button {
-    display: flex;
-
-    svg {
-      fill: $accent;
-      transition: 0.3s;
+        @media(max-width: 550px) {
+            box-shadow: none;
+            margin: 0;
+            max-width: 100%;
+            min-height: 100vh;
+        }
     }
 
-    &:hover {
-      svg {
-        transform: rotate(45deg);
-      }
-    }
-  }
+    .weather {
+        flex: 1;
+        margin-top: 16px;
+        padding-bottom: 16px;
+        position: relative;
 
-  .refresh {
-    div {
-      align-items: center;
-      display: flex;
+        @media(max-width: 850px) {
+            padding-bottom: 48px;
+        }
+
+        .current,
+        .forecast {
+            flex: 1;
+
+            @keyframes fadeIn {
+                0% {
+                    opacity: 0;
+                }
+                100% {
+                    opacity: 1;
+                }
+            }
+
+            svg {
+                height: auto;
+                width: 100%;
+            }
+        }
     }
-    
-    span {
-      color: $accent;
-      font-size: 14px;
-      margin-left: 5px;
+
+    .loading-or-error {
+        align-items: center;
+        flex: 1;
+        justify-content: center;
+        position: relative;
+
+        .spinner {
+            margin: -40px auto 0;
+        }
+
+        span {
+            font-size: 16px;
+            height: 16px;
+            margin-top: -26px;
+            position: absolute;
+            top: 50%;
+        }
+
+        &.loading span {
+            margin-top: 14px;
+        }
     }
-  }
-}
+
+    .footer {
+        align-items: center;
+        bottom: 0;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        left: 0;
+        margin: 0;
+        opacity: .75;
+        padding: 10px;
+        position: absolute;
+        width: 100%;
+
+        button {
+            display: flex;
+
+            svg {
+                fill: $accent;
+                transition: 0.3s;
+            }
+
+            &:hover {
+                svg {
+                    transform: rotate(45deg);
+                }
+            }
+        }
+
+        .refresh {
+            div {
+                align-items: center;
+                display: flex;
+            }
+
+            span {
+                color: $accent;
+                font-size: 14px;
+                margin-left: 5px;
+            }
+        }
+    }
 </style>
--- src/components/Current.vue
+++ src/components/Current.vue
@@ -1,256 +1,258 @@
 <template>
-  <div class="current">
-    <h1 class="location">{{ store.geocode.formattedAddress }}</h1>
+    <div class="current">
+        <h1 class="location">{{ store.geocode.formattedAddress }}</h1>
 
-    <div class="row">
-      <div class="col main">
-        <div>{{ date(store.weather.currently.time * 1000, store.weather.timezone) }}</div>
-        <div>{{ store.weather.currently.summary }}</div>
-        <div class="icon-and-temperature">
-          <div class="icon">
-            <WeatherIcon :icon="store.weather.currently.icon"/>
-          </div>
-          <div class="temperature">
-            <div>{{ Math.round(store.weather.currently.temperature) }}</div>
-              <sup :class="store.units">
-                <button class="us" title="Switch to Fahrenheit" @click="changeUnits('us')">°F</button>
-                <button class="si" title="Switch to Celsius" @click="changeUnits('si')">°C</button>
-            </sup>
-          </div>
-        </div>
-      </div>
+        <div class="row">
+            <div class="col main">
+                <div>{{ date(store.weather.currently.time * 1000, store.weather.timezone) }}</div>
+                <div>{{ store.weather.currently.summary }}</div>
+                <div class="icon-and-temperature">
+                    <div class="icon">
+                        <WeatherIcon :icon="store.weather.currently.icon"/>
+                    </div>
+                    <div class="temperature">
+                        <div>{{ Math.round(store.weather.currently.temperature) }}</div>
+                        <sup :class="store.units">
+                            <button class="us" title="Switch to Fahrenheit" @click="changeUnits('us')">°F</button>
+                            <button class="si" title="Switch to Celsius" @click="changeUnits('si')">°C</button>
+                        </sup>
+                    </div>
+                </div>
+            </div>
 
-      <ul class="col details">
-        <li>
-          Precipitation: <strong>{{ toPercentage(store.weather.currently.precipProbability) }}%</strong>
-        </li>
-        <li>
-          Cloud Coverage: <strong>{{ toPercentage(store.weather.currently.cloudCover) }}%</strong>
-        </li>
-        <li>
-          Humidity: <strong>{{ toPercentage(store.weather.currently.humidity) }}%</strong>
-        </li>
-        <li>
-          Dew Point: <strong>{{ Math.round(store.weather.currently.dewPoint) }}° {{ dewPointLabel }}</strong>
-        </li>
-        <li>
-          Wind: <strong>{{ store.weather.currently.windSpeed }} {{ windSpeedLabel }}</strong>
-        </li>
-        <li>
-          Visibility: <strong>{{ store.weather.currently.visibility }} {{ visibilityLabel }}</strong>
-        </li>
-        <li>
-          Sunrise: <strong>{{ timestamp(store.weather.daily.data[0].sunriseTime * 1000, store.weather.timezone) }}</strong>
-        </li>
-        <li>
-          Sunset: <strong>{{ timestamp(store.weather.daily.data[0].sunsetTime * 1000, store.weather.timezone) }}</strong>
-        </li>
-      </ul>
-    </div> <!-- end .row -->
-  </div>
+            <ul class="col details">
+                <li>
+                    Precipitation: <strong>{{ toPercentage(store.weather.currently.precipProbability) }}%</strong>
+                </li>
+                <li>
+                    Cloud Coverage: <strong>{{ toPercentage(store.weather.currently.cloudCover) }}%</strong>
+                </li>
+                <li>
+                    Humidity: <strong>{{ toPercentage(store.weather.currently.humidity) }}%</strong>
+                </li>
+                <li>
+                    Dew Point: <strong>{{ Math.round(store.weather.currently.dewPoint) }}° {{ dewPointLabel }}</strong>
+                </li>
+                <li>
+                    Wind: <strong>{{ store.weather.currently.windSpeed }} {{ windSpeedLabel }}</strong>
+                </li>
+                <li>
+                    Visibility: <strong>{{ store.weather.currently.visibility }} {{ visibilityLabel }}</strong>
+                </li>
+                <li>
+                    Sunrise: <strong>{{ timestamp(store.weather.daily.data[0].sunriseTime * 1000, store.weather.timezone)
+                    }}</strong>
+                </li>
+                <li>
+                    Sunset: <strong>{{ timestamp(store.weather.daily.data[0].sunsetTime * 1000, store.weather.timezone)
+                    }}</strong>
+                </li>
+            </ul>
+        </div> <!-- end .row -->
+    </div>
 </template>
 
 <script>
-import WeatherIcon from './WeatherIcon'
-import moment from 'moment'
-import 'moment-timezone'
+  import WeatherIcon from './WeatherIcon'
+  import moment from 'moment'
+  import 'moment-timezone'
 
-export default {
-  name: 'current',
+  export default {
+    name: 'current',
 
-  components: {
-    WeatherIcon
-  },
-
-  computed: {
-    store () {
-      return this.$store.state
+    components: {
+      WeatherIcon
     },
 
-    dewPointLabel () {
-      switch (this.store.units) {
-        case 'us':
-          return 'F'
-        case 'si':
-          return 'C'
+    computed: {
+      store () {
+        return this.$store.state
+      },
+
+      dewPointLabel () {
+        switch (this.store.units) {
+          case 'us':
+            return 'F'
+          case 'si':
+            return 'C'
+        }
+      },
+
+      visibilityLabel () {
+        switch (this.store.units) {
+          case 'us':
+            return 'miles'
+          case 'si':
+            return 'km'
+        }
+      },
+
+      windSpeedLabel () {
+        switch (this.store.units) {
+          case 'us':
+            return 'mph'
+          case 'si':
+            return 'kph'
+        }
       }
     },
 
-    visibilityLabel () {
-      switch (this.store.units) {
-        case 'us':
-          return 'miles'
-        case 'si':
-          return 'km'
+    methods: {
+      changeUnits (units) {
+        this.$store.dispatch('units', units)
+        this.$store.dispatch('appStatus', {state: 'loading'})
+        this.$store.dispatch('weather').then(() => {
+          this.$store.dispatch('appStatus', {state: 'loaded'})
+        })
+      },
+
+      date (time, zone) {
+        return moment(time).tz(zone).format('dddd, MMMM Do')
+      },
+
+      timestamp (time, zone) {
+        return moment(time).tz(zone).format('h:mm A')
+      },
+
+      toPercentage (value) {
+        return Math.round(value * 100)
       }
-    },
-
-    windSpeedLabel () {
-      switch (this.store.units) {
-        case 'us':
-          return 'mph'
-        case 'si':
-          return 'kph'
-      }
-    }
-  },
-
-  methods: {
-    changeUnits (units) {
-      this.$store.dispatch('units', units)
-      this.$store.dispatch('appStatus', { state: 'loading' })
-      this.$store.dispatch('weather').then(() => {
-        this.$store.dispatch('appStatus', { state: 'loaded' })
-      })
-    },
-
-    date (time, zone) {
-      return moment(time).tz(zone).format('dddd, MMMM Do')
-    },
-
-    timestamp (time, zone) {
-      return moment(time).tz(zone).format('h:mm A')
-    },
-
-    toPercentage (value) {
-      return Math.round(value * 100)
     }
   }
-}
 </script>
 
 <style lang="scss">
-@import '../scss/_vars.scss';
+    @import '../scss/_vars.scss';
 
-.current {
-  font-size: 20px;
-  
-  .row {
-    display: flex;
-    
-    & + .row {
-      padding-top: 16px;
-    }
+    .current {
+        font-size: 20px;
 
-    @media(max-width: 850px) {
-      flex-direction: column;
-    }
+        .row {
+            display: flex;
 
-    .col {
-      flex: 1;
-    }
-  }
+            & + .row {
+                padding-top: 16px;
+            }
 
-  .location {
-    align-items: baseline;
-    display: flex;
-    font-size: 32px;
-    margin-bottom: 4px;
+            @media(max-width: 850px) {
+                flex-direction: column;
+            }
 
-    div span {
-      &::after {
-        content: ', '
-      }
-
-      &:last-child {
-        &::after {
-          content: ''
-        }
-      }
-    }
-
-    .weak {
-      color: $accentLight;
-      font-size: 26px;
-      margin-left: 8px;
-    }
-  }
-
-  .icon-and-temperature {
-    display: flex;
-    padding-top: 8px;
-  }
-
-  .icon {
-    height: 64px;
-    width: 64px;
-
-    svg {
-      fill: $accent;
-    }
-  }
-
-  .temperature {
-    display: flex;
-    margin-left: 8px;
-    margin-top: -8px;
-
-    div {
-      font-size: 64px;
-      font-weight: 300;
-    }
-
-    sup {
-      margin-top: 11px;
-
-      .us {
-        padding-right: 4px;
-      }
-
-      .si {
-        border-left: 1px solid #ddd;
-        padding-left: 4px;
-      }
-
-      &.us {
-        .us {
-          font-weight: bold;
-          pointer-events: none;
+            .col {
+                flex: 1;
+            }
         }
 
-        .si {
-          color: $accentLight;
+        .location {
+            align-items: baseline;
+            display: flex;
+            font-size: 32px;
+            margin-bottom: 4px;
+
+            div span {
+                &::after {
+                    content: ', '
+                }
+
+                &:last-child {
+                    &::after {
+                        content: ''
+                    }
+                }
+            }
+
+            .weak {
+                color: $accentLight;
+                font-size: 26px;
+                margin-left: 8px;
+            }
         }
-      }
 
-      &.si {
-        .si {
-          font-weight: bold;
-          pointer-events: none;
+        .icon-and-temperature {
+            display: flex;
+            padding-top: 8px;
         }
 
-        .us {
-          color: $accentLight;
+        .icon {
+            height: 64px;
+            width: 64px;
+
+            svg {
+                fill: $accent;
+            }
         }
-      }
 
-      span {
-        cursor: pointer;
-      }
+        .temperature {
+            display: flex;
+            margin-left: 8px;
+            margin-top: -8px;
+
+            div {
+                font-size: 64px;
+                font-weight: 300;
+            }
+
+            sup {
+                margin-top: 11px;
+
+                .us {
+                    padding-right: 4px;
+                }
+
+                .si {
+                    border-left: 1px solid #ddd;
+                    padding-left: 4px;
+                }
+
+                &.us {
+                    .us {
+                        font-weight: bold;
+                        pointer-events: none;
+                    }
+
+                    .si {
+                        color: $accentLight;
+                    }
+                }
+
+                &.si {
+                    .si {
+                        font-weight: bold;
+                        pointer-events: none;
+                    }
+
+                    .us {
+                        color: $accentLight;
+                    }
+                }
+
+                span {
+                    cursor: pointer;
+                }
+            }
+        }
+
+        .details {
+            display: flex;
+            flex-wrap: wrap;
+            font-size: 15px;
+            line-height: 1;
+            padding-top: 8px;
+
+            @media(max-width: 850px) {
+                padding-top: 0;
+            }
+
+            li {
+                margin-top: 0;
+                text-align: right;
+                width: 50%;
+
+                @media(max-width: 850px) {
+                    margin-top: 8px;
+                    text-align: left;
+                }
+            }
+        }
     }
-  }
-
-  .details {
-    display: flex;
-    flex-wrap: wrap;
-    font-size: 15px;
-    line-height: 1;
-    padding-top: 8px;
-
-    @media(max-width: 850px) {
-      padding-top: 0;
-    }
-
-    li {
-      margin-top: 0;
-      text-align: right;
-      width: 50%;
-
-      @media(max-width: 850px) {
-        margin-top: 8px;
-        text-align: left;
-      }
-    }
-  }
-}
 </style>
