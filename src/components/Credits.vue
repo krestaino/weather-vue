7f8454936ba2f76c105b285e51e870fed8565914
Kevin Restaino <kevinrestaino@gmail.com>
2018-04-10 09:46:51 -0500

    copy(Credits): new copy and minor layout change

--- src/components/Credits.vue
+++ src/components/Credits.vue
@@ -1,27 +1,17 @@
 <template>
   <div class="credits">
-    <p>Weather Vue is made using the following technologies.</p>
-    <ul>
-      <li>
-        <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue.js</a>
-      </li>
-      <li>
-        <a href="https://darksky.net/dev/" target="_blank" rel="noopener">Dark Sky API</a>
-      </li>
-      <li>
-        <a href="https://developers.google.com/maps/documentation/javascript/get-api-key" target="_blank" rel="noopener">Google Maps JavaScript API</a>
-      </li>
-      <li>
-        <a href="https://developers.google.com/maps/documentation/geocoding/get-api-key" target="_blank" rel="noopener">Google Geocoding API</a>
-      </li>
-      <li>
-          <a href="https://developers.google.com/places/web-service/autocomplete" target="_blank" rel="noopener">Google Places API</a>
-      </li>
-    </ul>
-
-    <p class="source">
-      Grab the <a href="https://github.com/krestaino/weather-vue/" target="_blank" rel="noopener">source</a>.
-    </p>
+    <div>
+      <span>Weather Vue is built with</span>
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue.js</a>,
+      <a href="https://darksky.net/dev/" target="_blank" rel="noopener">Dark Sky API</a>,
+      <a href="https://developers.google.com/maps/documentation/javascript/get-api-key" target="_blank" rel="noopener">Google Maps JavaScript API</a>,
+      <a href="https://developers.google.com/maps/documentation/geocoding/get-api-key" target="_blank" rel="noopener">Google Geocoding API</a>,
+      <a href="https://developers.google.com/places/web-service/autocomplete" target="_blank" rel="noopener">Google Places API</a>,
+      and many other great packages.
+      <p class="source">
+        Grab the <a href="https://github.com/krestaino/weather-vue/" target="_blank" rel="noopener">source</a>.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -43,11 +33,15 @@ export default {
   animation-fill-mode: forwards;
   animation-iteration-count: 1;
   animation-name: fadeIn;
-  display: flex;
   flex: 1;
   height: 100%;
   justify-content: center;
+  line-height: 2;
   text-align: center;
+
+  > div {
+    max-width: 420px;
+  }
 }
 
 p {
