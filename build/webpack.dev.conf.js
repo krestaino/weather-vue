e85547d9dcf8fab6d6b019a0d46bcd2760626142
Ru Chern Chong <me@ruchern.com>
2018-04-12 00:29:52 +0800

    Update webpack config
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -14,7 +14,7 @@ Object.keys(baseWebpackConfig.entry).forEach(function (name) {
 module.exports = merge(baseWebpackConfig, {
   mode: 'development',
   module: {
-    rules: utils.styleLoaders({sourceMap: config.dev.cssSourceMap})
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
